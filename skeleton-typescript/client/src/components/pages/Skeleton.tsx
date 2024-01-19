ecf9212d6e7f0f1c229de1785db3a760af1dc7df
saulvegasauceda <86434516+saulvegasauceda@users.noreply.github.com>
2024-01-19 01:01:27 -0500

    added navbar and sidebar to editor

--- skeleton-typescript/client/src/components/pages/Skeleton.tsx
+++ skeleton-typescript/client/src/components/pages/Skeleton.tsx
@@ -7,7 +7,6 @@ import NavBar from "../NavBar";
 import { RouteComponentProps } from "@reach/router";
 import SideBar from "../SideBar";
 
-//TODO(weblab student): REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "969378506162-5ltmi54vc1d96lelg5b44slm8c1vbol3.apps.googleusercontent.com";
 
 type Props = RouteComponentProps & {
--- skeleton-typescript/client/src/components/pages/TextEditor.tsx
+++ skeleton-typescript/client/src/components/pages/TextEditor.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GoogleOAuthProvider, CredentialResponse } from "@react-oauth/google";
 
 import "./Skeleton.css";
 import "../../../src/output.css";
@@ -10,16 +11,40 @@ import Editor from "../Editor";
 //TODO(weblab student): REPLACE WITH YOUR OWN CLIENT_ID
 //const GOOGLE_CLIENT_ID = "969378506162-5ltmi54vc1d96lelg5b44slm8c1vbol3.apps.googleusercontent.com";
 
-type Props = RouteComponentProps 
-const Skeleton = (props: Props) => {
-  
+const GOOGLE_CLIENT_ID = "969378506162-5ltmi54vc1d96lelg5b44slm8c1vbol3.apps.googleusercontent.com";
+
+type Props = RouteComponentProps & {
+  userId?: string;
+  handleLogin: (credentialResponse: CredentialResponse) => void;
+  handleLogout: () => void;
+};
+const TextEditor = (props: Props) => {
+  const { handleLogin, handleLogout } = props;
 
   return (
-    <div>
-      <h1>Hello Llamas, this is your soon to be text editor, no CSS yet:o</h1>
-      <Editor/>
-    </div>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <div>
+        <header className="fixed top-0 z-50">
+          <NavBar handleLogin={handleLogin} handleLogout={handleLogout} userId={props.userId} />
+        </header>
+        <div className="sidebar-content-container">
+          <aside className="sidebar-page-container">
+            <SideBar />
+          </aside>
+          <section className="header-content-container">
+            <header className="header-container">
+              <h1 className="header-text">MemOurs</h1>
+            </header>
+            <main>
+              <div className="main-content-container">
+                <Editor />
+              </div>
+            </main>
+          </section>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
   );
 };
 
-export default Skeleton;
+export default TextEditor;
