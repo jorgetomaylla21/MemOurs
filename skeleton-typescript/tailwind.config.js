5c7aa17269bb1bd44141301ae71dc1fd1e6833c2
saulvegasauceda <86434516+saulvegasauceda@users.noreply.github.com>
2024-01-21 19:53:20 -0500

    primitive dark mode for text editor

--- skeleton-typescript/tailwind.config.js
+++ skeleton-typescript/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  darkMode: true,
+  darkMode: "media",
   content: [
     "./client/src/**/*.{html,js,ts,tsx}",
     "./client/src/components/**/*.{html,js,ts,tsx}",
