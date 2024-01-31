785e3dd0c09b9e9286afb08c4843c92cbc09fae7
Jorge Tomaylla <jorgetomaylla21@gmail.com>
2024-01-30 22:10:16 -0500

    Added Cat profile pic, will add more options later

--- client/src/components/modules/MenuItems/ProfileCard.tsx
+++ client/src/components/modules/MenuItems/ProfileCard.tsx
@@ -11,11 +11,12 @@ const ProfileCard = (props: Props) => {
     const display = props.userName ?? "No user";
     setUserName(display);
   }, [props.userName]);
+  const myImage = require('../../../assets/profile_cat.jpg');
 
   return (
     <section className="px-4 pb-8">
-      <div className="profilecard-container">
-        <div className="profile-pic"></div>
+      <div className="profilecard-container">       
+        <img src={myImage.default} alt="Profile Cat" className="profile-pic"/>       
       </div>
       <div className="profile-name">{userName}</div>
     </section>
